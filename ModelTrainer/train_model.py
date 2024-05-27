cdefc9f55c4850be838997164088da2c2db16b14
HerQTheToxic <herkules.marton.gergely@gmail.com>
2024-05-27 20:45:55 +0200

    HELP

--- ModelTrainer/train_model.py
+++ ModelTrainer/train_model.py
@@ -15,7 +15,7 @@ def parse_opt():
     parser.add_argument('--epochs', type=int, default=100, help='total training epochs')
     parser.add_argument('--model', type=str, default='yolov5m', help='model to be used for training')
     parser.add_argument('--name', default='exp', help='name of the current project output')
-    parser.add_argument('--dataset_name', help='dataset name')
+    parser.add_argument('--dataset', help='dataset name')
     parser.add_argument('--outputdir', type=str, default='C:/yolov5', help='the folder where the result of the training inside the runs/train folder shall be saved')
     parser.add_argument('--workspace', type=str, default='msc-onlab-1', help='the Roboflow workspace containing the project')
     parser.add_argument('--project', type=str, default='person-so5ko', help='the id of the Roboflow project')
@@ -24,23 +24,32 @@ def parse_opt():
     return parser.parse_args()
 
 
+def perform_training(opt):
+    if opt.yolov8:
+        #Train model with YOLOv8
+        model = YOLO(f"{opt.model}.pt")
+        model.train(imgsz=opt.img, batch=opt.batch, epochs=opt.epochs, data=f"{dataset_location}/data.yaml", name=opt.name, patience=0)
+    else:
+        #Train model with YOLOv5
+        train.run(img=opt.img, batch=opt.batch, epochs=opt.epochs, data=f"{dataset_location}/data.yaml", cfg=f"{yolov5path}/models/{opt.model}.yaml", weights=f"{opt.model}.pt", name=opt.name)
+
+
+
 if __name__ == '__main__':
     opt = parse_opt()
+    global yolov5path
+    yolov5path = os.getenv('YOLOV5_PATH', 'C:/yolov5')
+    if opt.yolov8:
+        try:
+            from ultralytics import YOLO
+        except ModuleNotFoundError:
+            raise ModuleNotFoundError("""Failed to import 'YOLO' from module 'ultralytics'. Maybe you forgot to install YOLOv8 using the following command: 'pip install ultralytics'.""")
+    else:
+        sys.path.append(yolov5path)
+        try:
+            import train
+        except ModuleNotFoundError:
+            raise ModuleNotFoundError("""Failed to import module 'train'. If your YOLOv5 is not located at 'C:\yolov5' maybe you forgot to set the YOLOV5_PATH system environment variable.""")
 
-    if not os.path.isdir(opt.YOLOv5_path):
-        raise FileNotFoundError("YOLOv5_path is not valid")  
-
-    sys.path.append(opt.YOLOv5_path) 
-    try:
-        import train
-    except ModuleNotFoundError:
-        raise ModuleNotFoundError("Failed to import module 'train'")
-    
-    
-    if not os.path.isdir(f"{opt.YOLOv5_path}/{opt.dataset_name}"):
-        raise FileNotFoundError(f"The dataset is not in the YOLOv5 directory: {opt.YOLOv5_path}/{opt.dataset_name}")
-    
-    
-    train.run(img=opt.img, batch=opt.batch, epochs=opt.epochs, data=f"{opt.YOLOv5_path}/{opt.dataset_name}/data.yaml", cfg=f"{opt.YOLOv5_path}/models/{opt.model}.yaml", weights=f"{opt.model}.pt", name=opt.name)
-
-
+    perform_training(opt)
+    #results.show_tensorboard(opt.outputdir, 8 if opt.yolov8 else 5)
